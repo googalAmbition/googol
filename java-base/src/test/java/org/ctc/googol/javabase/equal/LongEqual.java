442aaf9b7f14e1aa1a5f7564c2ef56112917aef6
taylor <13356896924@163.com>
2022-10-09 17:37:41 +0800

    java base

--- java-base/src/test/java/org/ctc/googol/javabase/equal/LongEqual.java
+++ java-base/src/test/java/org/ctc/googol/javabase/equal/LongEqual.java
@@ -4,29 +4,29 @@ import org.junit.Test;
 
 public class LongEqual {
 
-  String string = "   {\n" +
-    "            \"element_type\": \"service_task\", \n" +
-    "            \"id\": 17, \n" +
-    "            \"isEditing\": false, \n" +
-    "            \"defineText\": \"action\", \n" +
-    "            \"text\": \"Add a Task\", \n" +
-    "            \"noteText\": \"\", \n" +
-    "            \"nodeDepth\": 1, \n" +
-    "            \"iconType\": \"icon-task_add action\", \n" +
-    "            \"fieldExtensions\": [\n" +
-    "                {\n" +
-    "                    \"element_type\": \"field_extension\", \n" +
-    "                    \"fieldName\": \"taskId\", \n" +
-    "                    \"stringValue\": \"76\"\n" +
-    "                }\n" +
-    "            ], \n" +
-    "            \"implementation\": \"add_task\"\n" +
-    "        }";
+    String string = "   {\n" +
+        "            \"element_type\": \"service_task\", \n" +
+        "            \"id\": 17, \n" +
+        "            \"isEditing\": false, \n" +
+        "            \"defineText\": \"action\", \n" +
+        "            \"text\": \"Add a Task\", \n" +
+        "            \"noteText\": \"\", \n" +
+        "            \"nodeDepth\": 1, \n" +
+        "            \"iconType\": \"icon-task_add action\", \n" +
+        "            \"fieldExtensions\": [\n" +
+        "                {\n" +
+        "                    \"element_type\": \"field_extension\", \n" +
+        "                    \"fieldName\": \"taskId\", \n" +
+        "                    \"stringValue\": \"76\"\n" +
+        "                }\n" +
+        "            ], \n" +
+        "            \"implementation\": \"add_task\"\n" +
+        "        }";
 
-  @Test
-  public void newAndNew(){
-    Long long1_1 = 12L;
-    long long2_2 = 12L;
-    Long long3_3 = new Long(12L);
-  }
+    @Test
+    public void newAndNew() {
+        Long long1_1 = 12L;
+        long long2_2 = 12L;
+        Long long3_3 = new Long(12L);
+    }
 }
--- java-base/src/test/java/org/ctc/googol/javabase/set/SetContainsTest.java
+++ java-base/src/test/java/org/ctc/googol/javabase/set/SetContainsTest.java
@@ -1,6 +1,5 @@
 package org.ctc.googol.javabase.set;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,28 +9,27 @@ import java.util.Set;
 
 public class SetContainsTest {
 
-  private Set<String> setA = new HashSet<>();
+    private Set<String> setA = new HashSet<>();
 
-  private Set<String> setB = new HashSet<>();
+    private Set<String> setB = new HashSet<>();
 
-  @Before
-  public void initSet(){
-    setA.add("A");
-    setA.add("B");
-    setA.add("C");
-    setA.add("D");
-    setA.add("E");
+    @Before
+    public void initSet() {
+        setA.add("A");
+        setA.add("B");
+        setA.add("C");
+        setA.add("D");
+        setA.add("E");
 
-    setB.add("A");
-    setB.add("B");
-    setB.add("C");
-    // setB.add("F");
-  }
+        setB.add("A");
+        setB.add("B");
+        setB.add("C");
+        // setB.add("F");
+    }
 
-  @Test
-  public void containAllTest(){
-    Assert.assertTrue(
-      setA.containsAll(setB));
-  }
-
+    @Test
+    public void containAllTest() {
+        Assert.assertTrue(
+            setA.containsAll(setB));
+    }
 }
