d31ad60b61196dfe7b62b5af75ad111d1f2a0fd7
taylor <13356896924@163.com>
2022-11-17 19:31:08 +0800

    限流demo

---
+++ limit/src/main/java/com/tcoding/demo/limit/LimitApplication.java
@@ -0,0 +1,12 @@
+package com.tcoding.demo.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitApplication.class, args);
+    }
+}
---
+++ limit/src/test/java/com/tcoding/demo/limit/LimitApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tcoding.demo.limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
